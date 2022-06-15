2cfca7f008b868a073d61a7d98e90de65fef5968
Prantika Biswas <77984606+Prantika71@users.noreply.github.com>
2022-06-15 16:54:56 +0600

    Add files via upload
    Starting

---
+++ 1st_program.py
@@ -0,0 +1,19 @@
+# First python program
+print("90")
+a = 80
+b = "Sneha"
+c = 0.9
+d = " Joy Gopal "
+print(d)
+print(a)
+print(c)
+print(b)
+
+
+# local scope in python
+def demo():
+    a = 50
+    print(a)
+    demo()
+
+

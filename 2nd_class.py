49613cf7ec7c0091497da41310d734eaf04d13ec
Prantika Biswas <77984606+Prantika71@users.noreply.github.com>
2022-06-29 10:41:32 +0600

    Add files via upload
    2nd class (global scope)

---
+++ 2nd_class.py
@@ -0,0 +1,11 @@
+# global scope
+    f = 7.8
+def GLOBAL():
+    global f
+    f = 8.9
+    print(f)
+
+GLOBAL()
+print(f)
+
+# why error? f er value age use krchi then global scope declare krlam..bt r ekta same emney krchioikhn  a kono error dkhailo na
